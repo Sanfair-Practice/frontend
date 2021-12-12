bb6e982ec1c3db6ead8ce7f9bfc89efe2dda1b7d
Eugene Lukyantsau <pro100.freeman@gmail.com>
2021-12-12 13:39:56 +0300

    Improve Navigation UX

--- app/src/Components/Navigation.tsx
+++ app/src/Components/Navigation.tsx
@@ -1,5 +1,5 @@
 import {useUser} from "../Contexts";
-import {AppBar, Box, Button, Container, Toolbar} from "@mui/material";
+import {AppBar, Box, Button, ButtonGroup, Container, Toolbar} from "@mui/material";
 import {Link as RouteLink} from "react-router-dom";
 import React, {FC} from "react";
 
@@ -11,15 +11,15 @@ export const Navigation: FC = () => {
     }
 
     const mainLinks = [];
-    mainLinks.push(<Button key="home" component={RouteLink} to={"/"} sx={{color: "white"}}> Home </Button>);
+    mainLinks.push(<Button variant="outlined" color="inherit" key="home" component={RouteLink} to={"/"} sx={{color: "white"}}> Home </Button>);
 
     const secondaryLinks = [];
     if (user) {
-        secondaryLinks.push(<Button key="sign-out"  sx={{color: "white"}} onClick={handleSignOut}> Sign out </Button>);
+        secondaryLinks.push(<Button variant="outlined" color="inherit" key="sign-out"  sx={{color: "white"}} onClick={handleSignOut}> Sign out </Button>);
     }
     else {
-        secondaryLinks.push(<Button key="sign-in" component={RouteLink} to={"/sign-in"} sx={{color: "white"}}> Sign in </Button>);
-        secondaryLinks.push(<Button key="sign-up"  component={RouteLink} to={"/sign-up"} sx={{color: "white"}}> Sign up </Button>);
+        secondaryLinks.push(<Button variant="outlined" color="inherit" key="sign-in" component={RouteLink} to={"/sign-in"} sx={{color: "white"}}> Sign in </Button>);
+        secondaryLinks.push(<Button variant="outlined" color="inherit" key="sign-up"  component={RouteLink} to={"/sign-up"} sx={{color: "white"}}> Sign up </Button>);
     }
 
     return (
@@ -27,7 +27,11 @@ export const Navigation: FC = () => {
             <Container maxWidth="xl">
                 <Toolbar disableGutters>
                     <Box sx={{flexGrow: 1}}> {mainLinks} </Box>
-                    <Box sx={{flexGrow: 0}}> {secondaryLinks} </Box>
+                    <Box sx={{flexGrow: 0}}>
+                        <ButtonGroup>
+                            {secondaryLinks}
+                        </ButtonGroup>
+                    </Box>
                 </Toolbar>
             </Container>
         </AppBar>
